f85c01116e72637f2879405799605c0c4d0b9e66
vvatutin <161581923+vvatutin@users.noreply.github.com>
2024-03-16 15:00:53 +0500

    Add files via upload

--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -1,1 +1,22 @@
+from aiogram import types
 
+
+button1 = types.KeyboardButton(text='/start')
+button2 = types.KeyboardButton(text='/user')
+button3 = types.KeyboardButton(text='/stop')
+button4 = types.KeyboardButton(text='О нас')
+button5 = types.KeyboardButton(text='/prob')
+button6 = types.KeyboardButton(text='Обратный звонок')
+
+
+keyboard1 = [
+    [button1, button2, button3],
+    [button4, button5, button6],
+]
+
+keyboard2 = [
+    [button3, button4],
+]
+
+kb1 = types.ReplyKeyboardMarkup(keyboard=keyboard1, resize_keyboard=True)
+kb2 = types.ReplyKeyboardMarkup(keyboard=keyboard2, resize_keyboard=True)
